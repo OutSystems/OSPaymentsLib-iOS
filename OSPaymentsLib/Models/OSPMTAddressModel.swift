c37435fce0f884665453cab54d368168660cfbd0
Ricardo Silva <97543217+OS-ricardomoreirasilva@users.noreply.github.com>
2022-08-10 10:15:14 +0100

    refactor: Add DocC documentation and minor fixes. (#4)
    Add DocC documentation.
    Add empty value check and mandatory fields when fetching configuration properties.

--- OSPaymentsLib/Models/OSPMTAddressModel.swift
+++ OSPaymentsLib/Models/OSPMTAddressModel.swift
@@ -1,3 +1,4 @@
+/// Address Information related to a Billing or Shipping Information
 struct OSPMTAddressModel: Codable, Equatable {
     let postalCode: String
     let fullAddress: String
@@ -6,10 +7,19 @@ struct OSPMTAddressModel: Codable, Equatable {
     let administrativeArea: String?
     let state: String?
     
+    /// Keys used to encode and decode the model.
     enum CodingKeys: String, CodingKey {
         case postalCode, fullAddress, countryCode, city, administrativeArea, state
     }
     
+    /// Constructor method
+    /// - Parameters:
+    ///   - postalCode: Zip code.
+    ///   - fullAddress: Text containing full address (e.g. street, door number, floor, ...)
+    ///   - countryCode: ISO 3166-1 country code representation of the country.
+    ///   - city: City.
+    ///   - administrativeArea: Administrative Area, if existing.
+    ///   - state: State, if existing.
     init(postalCode: String, fullAddress: String, countryCode: String, city: String, administrativeArea: String? = nil, state: String? = nil) {
         self.postalCode = postalCode
         self.fullAddress = fullAddress
@@ -19,6 +29,12 @@ struct OSPMTAddressModel: Codable, Equatable {
         self.state = state
     }
     
+    /// Creates a new instance by decoding from the given decoder.
+    ///
+    /// This initializer throws an error if reading from the decoder fails, or
+    /// if the data read is corrupted or otherwise invalid.
+    ///
+    /// - Parameter decoder: The decoder to read data from.
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let postalCode = try container.decode(String.self, forKey: .postalCode)
@@ -37,6 +53,15 @@ struct OSPMTAddressModel: Codable, Equatable {
         )
     }
     
+    /// Encodes this value into the given encoder.
+    ///
+    /// If the value fails to encode anything, `encoder` will encode an empty
+    /// keyed container in its place.
+    ///
+    /// This function throws an error if any values are invalid for the given
+    /// encoder's format.
+    ///
+    /// - Parameter encoder: The encoder to write data to.
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(postalCode, forKey: .postalCode)
